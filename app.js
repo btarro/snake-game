dd35e2434f727bfb8d76b649e593a5ed633dc51d
Bruce Tarro <brucetarro@gmail.com>
2021-01-24 16:10:53 -0700

    working snake; collision detection

--- app.js
+++ app.js
@@ -1,39 +1,10 @@
-// How will the user interact with our application?
-
-// Test Case 1
-// App loads with an instruction canvas overlay
-// Pressing any button starts the game
-
-// Test Case 2
-//  A user can control the 'worm' with arrow keys
-
-// Test Case 3
-// The worm will grow in length for each astreoid it eats
-
-// Test Case 4
-// The game is over if the worm collides with any wall
-// The game is over if the worm collides with itself
-
-// Test Case 5
-// The 'score' is increased by 10 for each asteroid eaten
-
-// Test Case 6
-// When an asteroid is eaten, a new one is randomly generated on the canvas
-
-// Test Case 7
-// When the game is over the hi-score is updated if the user beats the previous score
-
-// Test Case 8
-// When the game is over load a 'game over' canvas
-
 let canvas;
 let canvasContext;
-let wormX = 250;
-let wormY = 250;
+let wormX = 200;
+let wormY = 200;
 let wormHeight = 20;
 let wormLength = 20;
-let wormGrow = 20;
-let wormSpeed = 1;
+let wormSpeed = 5;
 let lastMove = "ArrowRight";
 
 document.addEventListener("keydown", (e) => {
@@ -53,9 +24,9 @@ document.addEventListener("keydown", (e) => {
     moveRight();
     lastMove = e.code;
   }
-  // Test for apple
+  // Test for apple DELETEME
   if (e.code === "KeyG") {
-    wormLength = wormLength + wormGrow;
+    wormBody.push({ posX: wormX, posY: wormY, wormHeight, wormLength });
   }
 });
 
@@ -72,54 +43,99 @@ window.onload = function () {
 
 function drawEverything() {
   colorRect(0, 0, canvas.width, canvas.height, "black");
-  worm(wormX, wormY, wormHeight, wormLength, "white");
+  //worm(wormX, wormY, wormHeight, wormLength, "white");
+  wormBody.forEach((wormBodyPart) => {
+    worm(wormBodyPart.posX, wormBodyPart.posY, wormHeight, wormLength, "white");
+  });
 }
+
 function moveEverything() {
-  //console.log(lastMove);
+  // REPLACE WITH FOR EACH DELETEME
   if (lastMove == "ArrowRight") {
-    wormX = wormX + wormSpeed;
+    if (wormX < canvas.width - wormHeight) {
+      wormX = wormX + wormSpeed;
+      wormBody.unshift({ posX: wormX, posY: wormY });
+      wormBody.pop();
+    } else {
+      console.log("right side collision");
+      lastMove = "";
+    }
   }
   if (lastMove == "ArrowLeft") {
-    wormX = wormX - wormSpeed;
+    if (wormX > 0) {
+      wormX = wormX - wormSpeed;
+      wormBody.unshift({ posX: wormX, posY: wormY });
+      wormBody.pop();
+    } else {
+      console.log("left side collision");
+      lastMove = "";
+    }
   }
+
   if (lastMove == "ArrowUp") {
-    wormY = wormY - wormSpeed;
+    if (wormY > 0) {
+      wormY = wormY - wormSpeed;
+      wormBody.unshift({ posX: wormX, posY: wormY });
+      wormBody.pop();
+    } else {
+      console.log("top collision");
+      lastMove = "";
+    }
   }
   if (lastMove == "ArrowDown") {
-    wormY = wormY + wormSpeed;
+    if (wormY < canvas.height - wormHeight) {
+      wormY = wormY + wormSpeed;
+      wormBody.unshift({ posX: wormX, posY: wormY });
+      wormBody.pop();
+    } else {
+      console.log("bottom collision");
+      lastMove = "";
+    }
   }
 }
+
+function moveUp() {
+  if (wormY > 0) {
+    wormY = wormY - wormLength;
+  }
+}
+
+function moveDown() {
+  if (wormY < canvas.height - wormHeight) {
+    wormY = wormY + wormLength;
+  }
+}
+
+function moveLeft() {
+  if (wormX > 0) {
+    wormX = wormX - wormLength;
+  }
+}
+
+function moveRight() {
+  if (wormX < canvas.width - wormHeight) {
+    wormX = wormX + wormLength;
+  }
+}
+// xx
+//xx
+// Let's move the worm information into an object
+let wormBody = [
+  { posX: 200, posY: 200, height: wormHeight, length: wormLength },
+  { posX: 220, posY: 200, height: wormHeight, length: wormLength },
+  { posX: 240, posY: 200, height: wormHeight, length: wormLength },
+];
 
 function worm(leftX, topY, width, height, drawColor) {
   canvasContext.fillStyle = drawColor;
   canvasContext.fillRect(leftX, topY, width, height);
 }
 
-// MOVEMENT ARROWS
-function moveUp() {
-  if (wormY > 0) {
-    wormY = wormY - wormLength;
-  }
-}
-function moveDown() {
-  if (wormY < canvas.height - wormHeight) {
-    wormY = wormY + wormLength;
-  }
-}
-function moveLeft() {
-  if (wormX > 0) {
-    wormX = wormX - wormLength;
-  }
-}
-function moveRight() {
-  if (wormX < canvas.width - wormHeight) {
-    wormX = wormX + wormLength;
-  }
-}
-
-// CARRY OVER - Drawing the Canvas
-
 function colorRect(leftX, topY, width, height, drawColor) {
   canvasContext.fillStyle = drawColor;
   canvasContext.fillRect(leftX, topY, width, height);
-} // X,Y
+}
+
+// Each segment assumes the position of the last segment
+// each draw advances amount based on variable.
+// each previous segement (array element) changes variables to segment ahead of itself.
