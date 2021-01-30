c707e8b06ab6b5d307bf31c9602d5c4fe9a5d30a
Bruce Tarro <brucetarro@gmail.com>
2021-01-30 14:09:32 -0700

    Logic and gamplay complete.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const wormGrow = 10;
 const canvas = document.getElementById("game-canvas");
 const canvasContext = canvas.getContext("2d");
 
+let getScore = document.getElementById("game-score");
+let getHighScore = document.getElementById("high-score");
 let gameEngine;
 let wormX = 200;
 let wormY = 200;
@@ -18,6 +20,10 @@ let spaceJunk = {};
 let wormBody = [
   { posX: 200, posY: 200, height: wormHeight, length: wormWidth },
 ];
+
+window.onload = function () {
+  start();
+};
 
 document.addEventListener("keydown", (e) => {
   if (e.code === "ArrowUp" && !isY) {
@@ -37,20 +43,49 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
-window.onload = function () {
-  start();
-};
-
 function runGame() {
   gameStarted = true;
+  getScore.textContent = "00000";
   newJunk();
 
   let framesPerSecond = 60;
   gameEngine = setInterval(function () {
     moveEverything();
-    collisionCheck();
     drawEverything();
+    collisionCheck();
   }, 1000 / framesPerSecond);
+}
+
+function moveEverything() {
+  if (gameStarted) {
+    if (lastMove == "ArrowUp") {
+      isX = false;
+      isY = true;
+      wormY = wormBody[0].posY - wormSpeed;
+      growWormBody();
+    }
+
+    if (lastMove == "ArrowDown") {
+      isX = false;
+      isY = true;
+      wormY = wormBody[0].posY + wormSpeed;
+      growWormBody();
+    }
+
+    if (lastMove == "ArrowLeft") {
+      isX = true;
+      isY = false;
+      wormX = wormBody[0].posX - wormSpeed;
+      growWormBody();
+    }
+
+    if (lastMove == "ArrowRight") {
+      isX = true;
+      isY = false;
+      wormX = wormBody[0].posX + wormSpeed;
+      growWormBody();
+    }
+  }
 }
 
 function drawEverything() {
@@ -71,87 +106,30 @@ function drawEverything() {
     spaceJunk.posY,
     spaceJunk.height,
     spaceJunk.width,
-    "coral"
+    "orange"
   );
 }
 
-function moveEverything() {
-  if (lastMove == "ArrowUp") {
-    isX = false;
-    isY = true;
-    if (wormY > 0) {
-      wormY = wormBody[0].posY - wormSpeed;
-      wormBody.unshift({
-        posX: wormX,
-        posY: wormY,
-        height: wormHeight,
-        width: wormWidth,
-      });
-      wormBody.pop();
-    } else {
-      console.log("top collision");
-      lastMove = "";
-    }
+function collisionCheck() {
+  if (gameStarted) {
+    selfCrash();
   }
-
-  if (lastMove == "ArrowDown") {
-    isX = false;
-    isY = true;
-    if (wormY < canvas.height - wormHeight) {
-      wormY = wormBody[0].posY + wormSpeed;
-      wormBody.unshift({
-        posX: wormX,
-        posY: wormY,
-        height: wormHeight,
-        width: wormWidth,
-      });
-      wormBody.pop();
-    } else {
-      console.log("bottom collision");
-      lastMove = "";
-    }
+  if (gameStarted) {
+    consumeJunk();
   }
-
-  if (lastMove == "ArrowLeft") {
-    isX = true;
-    isY = false;
-    if (wormX > 0) {
-      wormX = wormBody[0].posX - wormSpeed;
-      wormBody.unshift({
-        posX: wormX,
-        posY: wormY,
-        height: wormHeight,
-        width: wormWidth,
-      });
-      wormBody.pop();
-    } else {
-      console.log("left side collision");
-      lastMove = "";
-    }
-  }
-
-  if (lastMove == "ArrowRight") {
-    isX = true;
-    isY = false;
-    if (wormX < canvas.width - wormWidth) {
-      wormX = wormBody[0].posX + wormSpeed;
-      wormBody.unshift({
-        posX: wormX,
-        posY: wormY,
-        height: wormHeight,
-        width: wormWidth,
-      });
-      wormBody.pop();
-    } else {
-      console.log("right side collision");
-      lastMove = "";
-    }
+  if (gameStarted) {
+    wallCrash();
   }
 }
 
-function collisionCheck() {
-  selfCrash();
-  consumeJunk();
+function growWormBody() {
+  wormBody.unshift({
+    posX: wormX,
+    posY: wormY,
+    height: wormHeight,
+    width: wormWidth,
+  });
+  wormBody.pop();
 }
 
 function selfCrash() {
@@ -166,10 +144,6 @@ function selfCrash() {
     let segRight = wormBody[i].posX;
     let segTop = wormBody[i].posY;
 
-    if (!gameStarted) {
-      return;
-    }
-
     if (
       headTop > segBottom ||
       headRight < segLeft ||
@@ -177,8 +151,21 @@ function selfCrash() {
       headLeft > segRight
     ) {
     } else {
-      gameOver();
+      gameOver("* trying to eat yourself *");
+      return;
     }
+  }
+}
+
+function wallCrash() {
+  if (
+    wormX > canvas.width - wormWidth - 1 ||
+    wormX <= 0 ||
+    wormY > canvas.height - wormHeight - 1 ||
+    wormY <= 0
+  ) {
+    lastMove = "";
+    gameOver("* hitting walls *");
   }
 }
 
@@ -195,6 +182,7 @@ function consumeJunk() {
     headLeft > spaceJunk.posX + spaceJunk.width
   ) {
   } else {
+    score += 25;
     newJunk();
     scoring();
     let segments = 1;
@@ -214,30 +202,41 @@ function newJunk() {
   let newX = Math.floor(Math.random() * 475 + 1);
   let newY = Math.floor(Math.random() * 475 + 1);
 
-  if (!selfCrash()) {
-    spaceJunk = {
-      posX: newX,
-      posY: newY,
-      height: wormHeight,
-      width: wormWidth,
-    };
-  } else {
-    newJunk();
+  for (i = 0; i < wormBody.length; i++) {
+    let segBottom = wormBody[i].posY;
+    let segLeft = wormBody[i].posX;
+    let segRight = wormBody[i].posX;
+    let segTop = wormBody[i].posY;
+
+    if (
+      newX < segLeft + 20 &&
+      newX + 20 > segLeft &&
+      newY < segTop + 20 &&
+      newY + 20 > segTop
+    ) {
+      newJunk();
+    } else {
+      spaceJunk = {
+        posX: newX,
+        posY: newY,
+        height: wormHeight,
+        width: wormWidth,
+      };
+    }
   }
 }
 
 function scoring() {
-  score += 25;
   if (score > highScore) {
     highScore = score;
     let pad = "00000";
     let result = (pad + score).slice(-pad.length);
-    document.getElementById("game-score").textContent = result;
-    document.getElementById("high-score").textContent = result;
+    getScore.textContent = result;
+    getHighScore.textContent = result;
   } else {
     let pad = "00000";
     let result = (pad + score).slice(-pad.length);
-    document.getElementById("game-score").textContent = result;
+    getScore.textContent = result;
   }
 }
 
@@ -246,7 +245,7 @@ function start() {
   canvasContext.font = "18px Arcade Interlaced";
   canvasContext.textAlign = "center";
   canvasContext.fillStyle = "yellowgreen";
-  canvasContext.fillText("* Greetings Nope-Rope *", canvas.width / 2, 100);
+  canvasContext.fillText("* Greetings, Nope-Rope *", canvas.width / 2, 100);
   canvasContext.fillStyle = "white";
   canvasContext.font = "14px Arcade Interlaced";
   canvasContext.fillText("You are a Danger Noodle.", canvas.width / 2, 175);
@@ -273,7 +272,7 @@ function start() {
   canvasContext.fillText("* PRESS SPACE TO START *", canvas.width / 2, 450);
 }
 
-function gameOver() {
+function gameOver(type) {
   clearInterval(gameEngine);
   wormX = 200;
   wormY = 200;
@@ -281,16 +280,31 @@ function gameOver() {
   isX = false;
   isY = false;
   score = 0;
-  //highScore = 750;
   gameStarted = false;
   spaceJunk = {};
   wormBody = [{ posX: 200, posY: 200, height: wormHeight, length: wormWidth }];
 
   gameObj(0, 0, canvas.width, canvas.height, "black");
-  canvasContext.font = "24px Arcade Interlaced";
+  canvasContext.font = "36px Arcade Interlaced";
   canvasContext.textAlign = "center";
   canvasContext.fillStyle = "red";
   canvasContext.fillText("GAME OVER", canvas.width / 2, 100);
+
+  canvasContext.font = "18px Arcade Interlaced";
+  canvasContext.fillStyle = "lightgrey";
+  canvasContext.fillText(
+    `SCORE ${getScore.textContent}`,
+    canvas.width / 2,
+    150
+  );
+
+  canvasContext.font = "14px Arcade Interlaced";
+  canvasContext.fillText(`It turns out..`, canvas.width / 2, 250);
+  canvasContext.fillStyle = "coral";
+  canvasContext.fillText(`${type}`, canvas.width / 2, 290);
+  canvasContext.fillStyle = "lightgrey";
+  canvasContext.fillText("..is dangerous.", canvas.width / 2, 330);
+
   canvasContext.font = "18px Arcade Interlaced";
   canvasContext.fillStyle = "yellow";
   canvasContext.fillText("* PRESS SPACE TO TRY AGAIN *", canvas.width / 2, 450);
@@ -300,5 +314,3 @@ function gameObj(leftX, topY, width, height, drawColor) {
   canvasContext.fillStyle = drawColor;
   canvasContext.fillRect(leftX, topY, width, height);
 }
-
-// Tset high score
